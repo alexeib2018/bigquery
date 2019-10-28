078626c8055e8a36ec82ca799ece3de0a6c03ab3
Alexei Bushuev <alexeib.2019@gmail.com>
2019-10-28 09:53:21 +0000

    developed parsing user properties

--- cgi/preprocess_events.py
+++ cgi/preprocess_events.py
@@ -24,26 +24,85 @@ cursor.execute(query)
 colnames = {desc.name: desc.table_column-1 for desc in cursor.description}
 
 
-def timestamp2datetime(ts):
-    if ts is None:
+def timestamp2datetime(value):
+    if value is None:
         return 'null'
-    seconds = int(int(ts) / 1000000)
-    microseconds = int(ts) - seconds * 1000000
+    seconds = int(int(value) / 1000000)
+    microseconds = int(value) - seconds * 1000000
     result = datetime.utcfromtimestamp(seconds).strftime('%Y-%m-%dT%H:%M:%S') + '.%i' % microseconds
     return "'%s'" % result
+
+
+def timestamp2string(value):
+    if value is None:
+        return None
+    seconds = int(int(value) / 1000)
+    microseconds = int(value) - seconds * 1000
+    if microseconds:
+        sfract = str(microseconds)
+        if sfract[-1] == 0: sfract=sfract[:-1]
+        if sfract[-1] == 0: sfract=sfract[:-1]
+        result = datetime.utcfromtimestamp(seconds).strftime('%Y-%m-%d %H:%M:%S') + '.%s' % sfract
+    else:
+        result = datetime.utcfromtimestamp(seconds).strftime('%Y-%m-%d %H:%M:%S')
+    return "%s" % result
+
 
 def copy(value):
     if value is None:
         return 'null'
     return value
 
+
 def quote(value):
     if value is None:
         return 'null'
     return "'%s'" % value
 
-def copyJSON(value):
+
+def copy_json(value):
     sjson = json.dumps(value)
+    sjson = sjson.replace("'", "''")
+    return "'%s'" % sjson
+
+
+def parse_user_properties(arr):
+    result = {}
+
+    for row in arr:
+        key = row['key']
+        value = row['value']
+        if key == 'first_open_time':
+            result[ 'first_open_time' ]           = timestamp2string( value[ 'int_value' ] )
+            result[ 'first_open_time_set' ]       = value[ 'set_timestamp_micros' ]
+        elif key == 'ga_session_id':
+            result[ 'ga_session_id' ]             = value[ 'int_value' ]
+            result[ 'ga_session_id_set' ]         = value[ 'set_timestamp_micros' ]
+        elif key == 'ga_session_number':
+            result[ 'ga_session_number' ]         = value[ 'int_value' ]
+            result[ 'ga_session_number_set' ]     = value[ 'set_timestamp_micros' ]
+        elif key == 'backup_enabled':
+            result[ 'backup_enabled' ]            = value[ 'string_value' ]
+            result[ 'backup_enabled_set' ]        = value[ 'set_timestamp_micros' ]
+        elif key == 'notifications_enabled':
+            result[ 'notifications_enabled' ]     = value[ 'string_value' ]
+            result[ 'notifications_enabled_set' ] = value[ 'set_timestamp_micros' ]
+        elif key == 'default_bill_cushion':
+            result[ 'default_bill_cushion' ]      = value[ 'string_value' ]
+            result[ 'default_bill_cushion_set' ]  = value[ 'set_timestamp_micros' ]
+        elif key == 'default_pay_cushion':
+            result[ 'default_pay_cushion' ]       = value[ 'string_value' ]
+            result[ 'default_pay_cushion_set' ]   = value[ 'set_timestamp_micros' ]
+        elif key == 'currency':
+            result[ 'currency' ]                  = value[ 'string_value' ]
+            result[ 'currency_set' ]              = value[ 'set_timestamp_micros' ]
+        elif key == 'saving_entry_name':
+            result[ 'saving_entry_name' ]         = value[ 'string_value' ]
+            result[ 'saving_entry_name_set' ]     = value[ 'set_timestamp_micros' ]
+        else:
+            print('Unknown user properties key (%s)' % key)
+
+    sjson = json.dumps(result)
     sjson = sjson.replace("'", "''")
     return "'%s'" % sjson
 
@@ -60,7 +119,7 @@ for row in cursor:
     event_server_timestamp_offset = 'null' # row[ colnames['event_server_timestamp_offset'] ]
     user_id                       = 'null' # row[ colnames['user_id'] ]
     user_pseudo_id                = row[ colnames['user_pseudo_id'] ]
-    user_properties               = 'null' # row[ colnames['user_properties'] ]
+    user_properties               = row[ colnames['user_properties'] ]
     user_first_touch_timestamp    = row[ colnames['user_first_touch_timestamp'] ]
     user_ltv                      = 'null' # row[ colnames['user_ltv'] ]
     device                        = 'null' # row[ colnames['device'] ]
@@ -88,13 +147,13 @@ for row in cursor:
     query += "   %s," % event_server_timestamp_offset
     query += "   %s," % user_id
     query += "   %s," % quote( user_pseudo_id )
-    query += "   %s," % user_properties
+    query += "   %s," % parse_user_properties( user_properties )
     query += "   %s," % timestamp2datetime( user_first_touch_timestamp )
     query += "   %s," % user_ltv
     query += "   %s," % device
     query += "   %s," % geo
     query += "   %s," % app_info
-    query += "   %s," % copyJSON( traffic_source )
+    query += "   %s," % copy_json( traffic_source )
     query += "   %s," % stream_id
     query += "   %s," % platform
     query += "   %s)" % event_dimensions
